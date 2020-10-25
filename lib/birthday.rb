f75f8a5b0887371039a217da104b0eaef75275b0
Justin Movitz <movitzj1@montclair.edu>
2020-10-25 17:55:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,3 +13,9 @@ def happy_birthday(birthday_kids)
   end
   winner
 end
+
+def happy_birthday(birthday_kids)
+ birthday_kids.each do |kids_name, age|
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ end
+end
