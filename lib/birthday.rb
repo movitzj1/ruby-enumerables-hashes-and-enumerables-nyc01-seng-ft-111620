8de8e157f81cce480e361ab2f1bbc56e97317506
Justin Movitz <movitzj1@montclair.edu>
2020-10-25 18:07:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,21 +1,3 @@
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
-
-def cruise_ship(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-  winner
-end
-
 birthday_kids = {
  "Timmy" => 9,
  "Sarah" => 6,
